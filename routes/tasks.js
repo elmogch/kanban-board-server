38aa4945e9c7cd849c1359a7fb2754fa74b9bef3
Luis Gutiérrez <luis@q-ark.mx>
2025-04-13 02:58:37 -0600

    add cors

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,7 +4,7 @@ const controller = require('../controllers/tasks');
 router.get('/', function(req, res, next) {
     res.send([
         { name: "1", stage: 1 },
-        { name: "2", stage: 0 },
+        { name: "3", stage: 0 },
       ]);
   });
 
